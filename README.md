fab2a00775e465084a957e73f63d664ac5b2ad34
zhangkang1521 <zhangkang_pub@163.com>
2016-12-31 23:00:46 +0800

    struts2 注解

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# struts2Demo
+# struts2Demo 注解支持
+* http://blog.csdn.net/liujiahan629629/article/details/22696589
+* 需加入 struts2-convention-plugin.jar
--- src/main/java/org/zk/struts2Demo/action/UserAction.java
+++ src/main/java/org/zk/struts2Demo/action/UserAction.java
@@ -1,14 +1,27 @@
 package org.zk.struts2Demo.action;
 
 
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+
 /**
  * Created by Administrator on 2016/4/3.
  */
+@Namespace("/user")
 public class UserAction {
 
 
+    @Action(
+            value = "list",
+            results = {
+                    @Result(name = "success", location = "/view/user/userList.jsp"),
+                    @Result(name = "success2", location = "/view/user/userList2.jsp")
+            }
+
+    )
     public String list(){
-        return "success";
+        return "success2";
     }
 
     public String add(){
