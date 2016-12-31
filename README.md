8729225f760e813a0dc1a7b29d06075ab18a92fc
zhangkang1521 <zhangkang_pub@163.com>
2016-12-31 22:31:00 +0800

    struts2整合spring

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# struts2Demo
+# struts2Demo 2.3.28 整合spring
+* http://www.cnblogs.com/zrtqsk/p/3496332.html
+* spring-plugin 引入
+* strut.xml action中class属性写beanId
--- src/main/java/org/zk/struts2Demo/action/UserAction.java
+++ src/main/java/org/zk/struts2Demo/action/UserAction.java
@@ -1,11 +1,18 @@
 package org.zk.struts2Demo.action;
 
 
+import org.zk.struts2Demo.service.UserService;
+
 /**
  * Created by Administrator on 2016/4/3.
  */
 public class UserAction {
 
+    private UserService userService;
+
+    public UserAction(){
+        System.out.println("new UserAction()");
+    }
 
     public String list(){
         return "success";
@@ -15,4 +22,7 @@ public class UserAction {
         return "success";
     }
 
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
